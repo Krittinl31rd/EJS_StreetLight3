fdf9f762ba4f3f3bc99e2180889f1caf79ddc849
unknown <m4galolx@gmail.com>
2025-09-23 16:29:56 +0700

    update lastest

--- config/sequelize.js
+++ config/sequelize.js
@@ -8,9 +8,10 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize("IOTServer_Lekise", "sa", "Archi_123456", {
   host: "archismarthome.com",
+  // host: "127.0.0.1",
   dialect: "mssql",
   logging: false,
-  port: 11230
+  port: 11230,
 });
 
 const testConnection = async () => {
